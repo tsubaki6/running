6e3e66cb93413a1e7dd6aabfc226ac88299cbbaa
unknown <martyna.jablonska7@gmail.com>
2016-05-22 19:12:18 +0200

    dodana trasa

--- app/src/main/java/com/example/martyna/run/MapsActivity.java
+++ app/src/main/java/com/example/martyna/run/MapsActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -32,7 +33,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -40,13 +45,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
-
+    private ArrayList<LatLng> latlng;
     private GoogleMap mMap;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -98,7 +104,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnMyLocationChangeListener(myLocationChangeListener);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        latlng = new ArrayList<LatLng>();
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
@@ -106,37 +112,90 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("DEBUG", "Map clicked [" + point.latitude + " / " + point.longitude + "]");
                 mMap.addMarker(new MarkerOptions()
                         .position(new LatLng(point.latitude, point.longitude)));
+                latlng.add(point);
+                if(latlng.size()>1) {
+                   String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + latlng.get(0).latitude + "," + latlng.get(0).longitude + "&destination=" + latlng.get(1).latitude + "," + latlng.get(1).longitude + "&key=AIzaSyDHrKDpJ8oW6oyzbMW4RV5L2JhpSmfqffA&mode=walking";
+                   if(latlng.size()>2) {
+                       Log.d("DEBUG2", "Map clicked ["+ latlng.get(2).longitude +"]");
+                       url=url+"&waypoints=";
+                       int i;
+                       for (i = 2; i < latlng.size(); i++) {
+                            url=url+latlng.get(i).latitude+","+latlng.get(i).longitude+"|";
+                       }
+                       url = url.substring(0, url.length()-1);
+                       Log.d("DEBUG3", url);
+                       AsyncTask<String, Void, JSONObject> task = new JSONAsyncTask().execute(url);
+                   }
+                    AsyncTask<String, Void, JSONObject> task = new JSONAsyncTask().execute(url);
+                   //"https://maps.googleapis.com/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyDHrKDpJ8oW6oyzbMW4RV5L2JhpSmfqffA");
+                   JSONObject json2 = null;
+                   try {
+                       json2 = task.get();
+                       Context context = getApplicationContext();
+                       String text = json2.toString();
+                       final JSONObject json = new JSONObject(text);
+                       JSONArray routeArray = json.getJSONArray("routes");
+                       JSONObject routes = routeArray.getJSONObject(0);
+                       JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+                       String encodedString = overviewPolylines.getString("points");
+                       List<LatLng> list = decodePoly(encodedString);
+                       Polyline line = mMap.addPolyline(new PolylineOptions()
+                                       .addAll(list)
+                                       .width(12)
+                                       .color(Color.parseColor("#05b1fb"))//Google maps blue color
+                                       .geodesic(true)
+                       );
+                       int duration = Toast.LENGTH_SHORT;
 
-                try {
-                    URI website = new URI("https://maps.googleapis.com/maps/api/directions/json?origin=" + mMap.getMyLocation().getLatitude() + "%" + mMap.getMyLocation().getLongitude() + "&destination=" + point.latitude + "%" + point.longitude + "&key=AIzaSyDqqXN6o1-n9_FoboDGZxGM3sIdWDXv_Fs&mode=walking");
-                    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + mMap.getMyLocation().getLatitude() + "%" + mMap.getMyLocation().getLongitude() + "&destination=" + point.latitude + "%" + point.longitude + "&key=AIzaSyDHrKDpJ8oW6oyzbMW4RV5L2JhpSmfqffA&mode=walking";
-                    AsyncTask<String, Void, JSONObject> task= new JSONAsyncTask().execute("https://maps.googleapis.com/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyDHrKDpJ8oW6oyzbMW4RV5L2JhpSmfqffA");
-                    JSONObject json = null;
-                    try {
-                        json = task.get();
-                        Context context = getApplicationContext();
-                        CharSequence text = json.toString();
-                        int duration = Toast.LENGTH_SHORT;
-
-                        Toast toast = Toast.makeText(context, text, duration);
-                        toast.show();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                }
-
+                       Toast toast = Toast.makeText(context, text, duration);
+                       //toast.show();
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   } catch (ExecutionException e) {
+                       e.printStackTrace();
+                   } catch (JSONException e) {
+                       e.printStackTrace();
+                   }
+               }
                 //Do your stuff with LatLng here
                 //Then pass LatLng to other activity
             }
         });
     }
 
+    private List<LatLng> decodePoly(String encoded) {
 
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng( (((double) lat / 1E5)),
+                    (((double) lng / 1E5) ));
+            poly.add(p);
+        }
+
+        return poly;
+    }
 
     private void setUpMap() //If the setUpMapIfNeeded(); is needed then...
     {
